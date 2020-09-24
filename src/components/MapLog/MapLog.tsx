586c45a1207312c518ce975a0722c11a57d67e74
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2020-09-24 18:14:42 +0900

    moved player draggables and handling to be able to access map

--- src/components/MapLog/MapLog.tsx
+++ src/components/MapLog/MapLog.tsx
@@ -1,81 +1,42 @@
 import React from "react";
-import Draggable from "react-draggable";
 import { useData } from "context";
 import useStyles from "./MapLog.styles";
 import Button from "components/common/Button";
 import { formatRelative } from "date-fns";
-import { IPlayer } from "utils/types";
 
-export default function MapLog(): JSX.Element {
-  const {
-    mapLogs,
-    setMapLogs,
-    roundStartTimes,
-    setRoundStartTimes,
-    allPlayers,
-  } = useData()!; // eslint-disable-line
-  const [roundNumber, setRoundNumber] = React.useState(mapLogs.length - 1);
+export interface IMapLogProps {
+  roundNumber: number;
+  goBackRound: () => void;
+  goForwardRound: () => void;
+  startNewRound: () => void;
+}
+
+export default function MapLog({
+  roundNumber,
+  goBackRound,
+  goForwardRound,
+  startNewRound,
+}: IMapLogProps): JSX.Element {
+  const { mapLogs, roundStartTimes } = useData()!; // eslint-disable-line
 
   const currentRoundLogs = mapLogs.filter(({ round }) => round === roundNumber);
   const currentRoundStart = roundStartTimes[roundNumber];
 
   const classes = useStyles({});
 
-  const backRound = () => {
-    setRoundNumber(roundNumber - 1);
-  };
-
-  const forwardRound = () => {
-    setRoundNumber(roundNumber + 1);
-  };
-
-  const newRound = () => {
-    setRoundStartTimes([...roundStartTimes, Date.now()]);
-    setRoundNumber(roundStartTimes.length);
-  };
-
   const formatPosition = ({ x, y }: { x: number; y: number }) =>
     `at ${x}, ${y}`;
 
-  const addLog = (
-    player: IPlayer,
-    position: { x: number; y: number },
-    timestamp: number,
-    round: number
-  ) => {
-    if (roundNumber === -1) {
-      newRound();
-      setMapLogs([...mapLogs, { player, position, timestamp, round: 0 }]);
-    } else {
-      setMapLogs([...mapLogs, { player, position, timestamp, round }]);
-    }
-  };
-
   return (
     <div className={classes.root}>
-      {allPlayers.map((player) => (
-        <Draggable
-          key={player.id}
-          bounds="parent"
-          onStop={(e, { x, y }) =>
-            addLog(player, { x, y }, Date.now(), roundNumber)
-          }
-        >
-          <img
-            className="player-handle"
-            src={`assets/${player.id}.png`}
-            draggable={false}
-          />
-        </Draggable>
-      ))}
-      <Button className={classes.newRoundButton} onClick={newRound}>
+      <Button className={classes.newRoundButton} onClick={startNewRound}>
         New Round
       </Button>
       <div className={classes.roundSelectorContainer}>
         <Button
           className={classes.roundSelectorButton}
           disabled={roundNumber <= 0}
-          onClick={backRound}
+          onClick={goBackRound}
         >
           &lt;
         </Button>
@@ -85,7 +46,7 @@ export default function MapLog(): JSX.Element {
         <Button
           className={classes.roundSelectorButton}
           disabled={roundNumber === roundStartTimes.length - 1}
-          onClick={forwardRound}
+          onClick={goForwardRound}
         >
           &gt;
         </Button>
--- src/components/MapsContent/MapsContent.tsx
+++ src/components/MapsContent/MapsContent.tsx
@@ -1,12 +1,42 @@
 import React from "react";
+import Draggable from "react-draggable";
 import useStyles from "./MapsContent.styles";
 import FeedbackForm from "../FeedbackForm";
 import Button from "components/common/Button";
 import MapLog from "components/MapLog/MapLog";
+import { useData } from "context";
+import { IPlayer } from "utils/types";
 
 export default function MapsContent(): JSX.Element {
   const [map, setMap] = React.useState("skeld");
   const [currentTab, setCurrentTab] = React.useState("Maps");
+
+  const {
+    mapLogs,
+    setMapLogs,
+    roundStartTimes,
+    setRoundStartTimes,
+    allPlayers,
+  } = useData()!; // eslint-disable-line
+  const [roundNumber, setRoundNumber] = React.useState(mapLogs.length - 1);
+
+  const startNewRound = () => {
+    setRoundStartTimes([...roundStartTimes, Date.now()]);
+    setRoundNumber(roundStartTimes.length);
+  };
+  const addLog = (
+    player: IPlayer,
+    position: { x: number; y: number },
+    timestamp: number,
+    round: number
+  ) => {
+    if (roundNumber === -1) {
+      startNewRound();
+      setMapLogs([...mapLogs, { player, position, timestamp, round: 0 }]);
+    } else {
+      setMapLogs([...mapLogs, { player, position, timestamp, round }]);
+    }
+  };
 
   const classes = useStyles({
     map: map == "skeld" ? "TheSkeld" : map == "mira" ? "Mirahq" : "Polus",
@@ -66,9 +96,29 @@ export default function MapsContent(): JSX.Element {
               className={classes.map}
               draggable={false}
             />
+            {allPlayers.map((player) => (
+              <Draggable
+                key={player.id}
+                bounds="parent"
+                onStop={(_, { x, y }) =>
+                  addLog(player, { x, y }, Date.now(), roundNumber)
+                }
+              >
+                <img
+                  className="player-handle"
+                  src={`assets/${player.id}.png`}
+                  draggable={false}
+                />
+              </Draggable>
+            ))}
           </div>
           <div className={classes.wrapper}>
-            <MapLog />
+            <MapLog
+              roundNumber={roundNumber}
+              goBackRound={() => setRoundNumber(roundNumber - 1)}
+              goForwardRound={() => setRoundNumber(roundNumber + 1)}
+              startNewRound={startNewRound}
+            />
           </div>
         </>
       ) : (
